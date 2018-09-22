1ae5bfb55c0ea104a0bc1249f11d9c059a15c4e5
ifenna <jawagu@gmail.com>
2018-09-22 16:20:38 +0100

    Fix multiple notebook database creation

--- cmd/utils.go
+++ cmd/utils.go
@@ -3,12 +3,21 @@ package cmd
 import (
 	"github.com/uncultured/notes/models"
 	"log"
+	"os/user"
+	"path"
 )
 
-
+// TODO: configure a global path here because as it is,
+// a new db is created when the app is run from a diff folder
 func setupDatabase() models.Datastore {
 	var database models.Datastore
-	database, err := models.NewDB("notebooks.db")
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal( err )
+	}
+	notebook := path.Join(usr.HomeDir, ".notebooks.db")
+
+	database, err = models.NewDB(notebook)
 	if err != nil {
 		log.Panic(err)
 	}
