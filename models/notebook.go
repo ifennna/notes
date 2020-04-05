6f86fd85470a9c6b41dc119f0774d0d7fcc398ac
y2k-shubham <y2k.shubhamgupta@gmail.com>
2020-04-06 01:45:22 +0530

    [doc] (models/notebook) Document remaining methods

--- models/notebook.go
+++ models/notebook.go
@@ -47,12 +47,13 @@ func (db *DB) GetNotebook(reqName string) (Notebook, error) {
 	var notebook Notebook
 	notebook.Name = reqName
 	err := db.View(func(tx *bolt.Tx) error {
+		// TODO: play with strings instead of bytes
 		reqNameBytes := []byte(reqName)
 
 		bucket := tx.Bucket([]byte("Notebook"))
 		foundNameBytes, _ := bucket.Cursor().Seek(reqNameBytes)
 		if foundNameBytes != nil && bytes.Equal(reqNameBytes, foundNameBytes) {
-			notebook.Notes = getNotesInNotebook(bucket, foundNameBytes)
+			notebook.Notes = getNotesInNotebook(bucket, reqNameBytes)
 		}
 
 		return nil
@@ -60,38 +61,68 @@ func (db *DB) GetNotebook(reqName string) (Notebook, error) {
 	return notebook, err
 }
 
+/**
+ * Retrieves all notebooks (along with their notes)
+ *  - deletegates actual work to 'getNotebooksInRootBucket' function
+ * two better alternatives
+ *  - in current implementation GetNotebook() should use this call
+ *  - (better) modify this method to 'GetAllNotebookNames'
+ */
 func (db *DB) GetAllNotebooks() ([]Notebook, error) {
 	var notebooks []Notebook
 	err := db.View(func(tx *bolt.Tx) error {
 		bucket := tx.Bucket([]byte("Notebook"))
-		cursor := bucket.Cursor()
-		notebooks = getNotebooksInRootBucket(cursor, bucket, notebooks)
+		notebooks = getNotebooksInRootBucket(bucket.Cursor(), bucket)
+
 		return nil
 	})
 	return notebooks, err
 }
 
-func getNotebooksInRootBucket(cursor *bolt.Cursor, bucket *bolt.Bucket, notebooks []Notebook) []Notebook {
-	for key, _ := cursor.First(); key != nil; key, _ = cursor.Next() {
+/**
+ * Function wrapping the core logic of 'GetAllNotebooks'
+ *  - iterates over notebooks in bucket
+ *  - uses getNotesInNotebook() call to retrieve notes of notebooks
+ * param: *bolt.Cursor cursor
+ * param: *bolt.Bucket bucket
+ * return: []Notebook
+ */
+func getNotebooksInRootBucket(cursor *bolt.Cursor, bucket *bolt.Bucket) []Notebook {
+	var notebooks []Notebook
+	for notebookNameBytes, _ := cursor.First(); notebookNameBytes != nil; notebookNameBytes, _ = cursor.Next() {
 		var notebook Notebook
-		notebook.Notes = getNotesInNotebook(bucket, key)
-		notebook.Name = string(key)
+		notebook.Name = string(notebookNameBytes)
+		// TODO: remove notes retrieval from here as an optimization
+		notebook.Notes = getNotesInNotebook(bucket, notebookNameBytes)
 		notebooks = append(notebooks, notebook)
 	}
 	return notebooks
 }
 
-func getNotesInNotebook(bucket *bolt.Bucket, key []byte) []Note {
+/**
+ * Retrieves notes in a given notebook
+ * param: *bolt.Bucket bucket
+ * param: []byte       notebookNameBytes
+ * return: []Note
+ */
+func getNotesInNotebook(bucket *bolt.Bucket, notebookNameBytes []byte) []Note {
 	var notes []Note
-	nestedBucketCursor := bucket.Bucket([]byte(key)).Cursor()
-	for key, value := nestedBucketCursor.First(); key != nil; key, value = nestedBucketCursor.Next() {
+	nestedBucketCursor := bucket.Bucket([]byte(notebookNameBytes)).Cursor()
+	// TODO: leverage this thing and allow 'naming' notes within a notebook
+	for noteNameBytes, noteBodyBytes := nestedBucketCursor.First(); noteNameBytes != nil; noteNameBytes, noteBodyBytes = nestedBucketCursor.Next() {
 		var note Note
-		json.Unmarshal(value, &note)
+		json.Unmarshal(noteBodyBytes, &note)
 		notes = append(notes, note)
 	}
 	return notes
 }
 
+/**
+ * Prints all data of all notebooks
+ * Output can be piped into a file for persisting and sharing
+ *  - delegates actual work to 'dumpCursor' method
+ * TODO: add CLI interface to invoke this function
+ */
 func (db *DB) Dump() {
 	err := db.View(func(tx *bolt.Tx) error {
 		c := tx.Cursor()
@@ -104,6 +135,12 @@ func (db *DB) Dump() {
 	}
 }
 
+/**
+ * Recursively prints all data of given cursor
+ * param: *bolt.Tx     tx
+ * param: *bolt.Cursor c
+ * param: int          indent
+ */
 func dumpCursor(tx *bolt.Tx, c *bolt.Cursor, indent int) {
 	for k, v := c.First(); k != nil; k, v = c.Next() {
 		if v == nil {
